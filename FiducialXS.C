65f80974ea0f199525feca499f4776df37c8183b
piedraj <jonatan.piedra@gmail.com>
2015-05-22 18:36:11 +0200

    Removed unnecesary lines

--- FiducialXS.C
+++ FiducialXS.C
@@ -8,8 +8,6 @@
 #include <iostream>
 
 
-enum {NONE=-1, EE=0, MM=1, EM=2};
-
 TFile* output_file;
 
 TH1F* h_events;
@@ -229,8 +227,8 @@ void FiducialXS::Loop(Int_t index)
 	  
 	  if (T_JetAKCHS_Et->at(i) > 30. &&
 	      fabs(T_JetAKCHS_Eta->at(i)) < 2.4 &&
-	      (jet.DeltaR(vElec[0]) >= 0.4) &&
-	      (jet.DeltaR(vMuon[0]) >= 0.4) &&
+	      (jet.DeltaR(vElec[0]) > 0.4) &&
+	      (jet.DeltaR(vMuon[0]) > 0.4) &&
 	      passJetID(i)) {
 
             njet++;
@@ -262,39 +260,6 @@ void FiducialXS::Loop(Int_t index)
 
   // Write the output
   //----------------------------------------------------------------------------
-  float n_events                         = h_events                        ->Integral();
-  float n_ttbar                          = h_ttbar                         ->Integral();
-  float n_ttbar_selected                 = h_ttbar_selected                ->Integral();
-  float n_ttbar_emu                      = h_ttbar_emu                     ->Integral();
-  float n_ttbar_emu_fiducial             = h_ttbar_emu_fiducial            ->Integral();
-  float n_ttbar_emu_fiducial_selected    = h_ttbar_emu_fiducial_selected   ->Integral();
-  float n_ttbar_emu_nonfiducial_selected = h_ttbar_emu_nonfiducial_selected->Integral();
-
-  float eff      = (n_ttbar > 0) ? n_ttbar_selected / n_ttbar : -999;
-  float eff_fid  = (n_ttbar_emu_fiducial > 0) ? n_ttbar_emu_fiducial_selected / n_ttbar_emu_fiducial : -999;
-  float f        = (n_ttbar_emu_fiducial_selected > 0) ? n_ttbar_emu_nonfiducial_selected / n_ttbar_emu_fiducial_selected : -999;
-  float xs_ratio = eff / eff_fid / (1. + f);
-
-  printf("\n\n");
-  printf("--------------------------------------------------\n");
-  printf(" N(events)                             = %.0f\n", n_events);
-  printf("--------------------------------------------------\n");
-  printf(" N(ttbar inclusive)                    = %.0f\n", n_ttbar);
-  printf(" N(ttbar inclusive selected)           = %.0f\n", n_ttbar_selected);
-  printf(" N(ttbar -> emu selected)              = %.0f (not used)\n", n_ttbar_emu_fiducial_selected + n_ttbar_emu_nonfiducial_selected);
-  printf(" total efficiency eff                  = %5.2f%s\n", 1e2 * eff, "%");
-  printf("--------------------------------------------------\n");
-  printf(" N(ttbar -> emu)                       = %.0f\n", n_ttbar_emu);
-  printf(" N(ttbar -> emu fiducial)              = %.0f\n", n_ttbar_emu_fiducial);
-  printf(" N(ttbar -> emu fiducial selected)     = %.0f\n", n_ttbar_emu_fiducial_selected);
-  printf(" N(ttbar -> emu non-fiducial selected) = %.0f\n", n_ttbar_emu_nonfiducial_selected);
-  printf(" fiducial efficiency eff_fid           = %5.2f%s\n", 1e2 * eff_fid, "%");
-  printf(" contamination fraction f              = %5.2f%s\n", 1e2 * f, "%");
-  printf("--------------------------------------------------\n");
-  printf(" xs_fid/xs = eff/eff_fid/(1+f)         = %.4f\n", xs_ratio);
-  printf("--------------------------------------------------\n");
-  printf("\n");
-  
   output_file->Write();
   output_file->Close();
 }
